fb6c005f5ad8f8804dc007960d26f68d2cadd42b
R-devMaker <rei.forworkonly@gmail.com>
2020-02-03 13:31:41 +0700

    Tugas 2 JavaScript
    Pengumpulan tugas 2 JavaScript Niomic

---
+++ tugas2.js
@@ -0,0 +1,21 @@
+var angka = 20.88;
+var huruf = "abcdefghijklmnopqrstuvwxyz";
+var kondisi = true;
+var cm = null;
+var cm2;
+var hewan = ["gajah","singa","harimau","rusa","bebek","ayam"];
+
+console.log(angka);
+console.log(huruf);
+
+if (angka > 0) {
+    kondisi = true;
+} else {
+    kondisi = false;
+}
+
+console.log(kondisi);
+console.log(cm);
+console.log(cm2);
+console.log(hewan);
+console.log(hewan[1]);
